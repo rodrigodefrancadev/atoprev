0bbb3912829fa1665db89d0540c2344caa006a04
Rodrigo de França <rodrigodefrancadev@gmail.com>
2024-02-09 22:51:51 -0300

    added UniqueId

---
+++ lib/core/unique_id.dart
@@ -0,0 +1,9 @@
+import 'package:atoprev/core/value_object.dart';
+import 'package:atoprev/helpers/is_uuid_v4.dart';
+import 'package:uuid/uuid.dart';
+
+class UniqueId extends ValueObject<String> {
+  UniqueId([String? value])
+      : assert(value == null || isUuidV4(value), 'Id must be a valid UUID'),
+        super(value ?? const Uuid().v4());
+}
---
+++ test/core/unique_id_test.dart
@@ -0,0 +1,24 @@
+import 'package:atoprev/core/unique_id.dart';
+import 'package:atoprev/helpers/is_uuid_v4.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Tests for UniqueId', () {
+    test('should return a valid UUID', () {
+      final uniqueId = UniqueId();
+      final result = isUuidV4(uniqueId.value);
+      expect(result, true);
+    });
+
+    test('should be created succesfuly with a provided uuidv4 value', () {
+      const uuid = 'f47ac10b-58cc-4372-a567-0e02b2c3d479';
+      final uniqueId = UniqueId(uuid);
+      expect(uniqueId.value, uuid);
+    });
+
+    test('should throw an AssertionError if the provided uuidv4 value is invalid', () {
+      const uuid = 'f47ac10b-58cc-4372-a567-0e02b2c3d479z';
+      expect(() => UniqueId(uuid), throwsA(isA<AssertionError>()));
+    });
+  });
+}
