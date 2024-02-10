1b20bf724dcb3e37829efe0d118e0383f5229b48
Rodrigo de França <rodrigodefrancadev@gmail.com>
2024-02-09 22:43:14 -0300

    added helper function isUuidV4

---
+++ lib/helpers/is_uuid_v4.dart
@@ -0,0 +1,6 @@
+bool isUuidV4(String value) {
+  final RegExp uuid = RegExp(
+    r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$',
+  );
+  return uuid.hasMatch(value);
+}
---
+++ test/helpers/is_uuid_v4_test.dart
@@ -0,0 +1,18 @@
+import 'package:atoprev/helpers/is_uuid_v4.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Tests for isUuidV4 function', () {
+    test('should return true for valid UUID', () {
+      const String uuid = 'f47ac10b-58cc-4372-a567-0e02b2c3d479';
+      final result = isUuidV4(uuid);
+      expect(result, true);
+    });
+
+    test('should return false for invalid UUID', () {
+      const String uuid = 'f47ac10b-58cc-4372-a567-0e02b2c3d479z';
+      final result = isUuidV4(uuid);
+      expect(result, false);
+    });
+  });
+}
